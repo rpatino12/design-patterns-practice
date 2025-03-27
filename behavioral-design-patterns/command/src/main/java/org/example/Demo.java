45e4c3ff9a7405a3c14d7afb5bd99764494c66d0
Ricardo Patino <rpatino12@outlook.com>
2025-03-27 14:04:42 -0500

    Command pattern

---
+++ behavioral-design-patterns/command/src/main/java/org/example/Demo.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.editor.Editor;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
